1677b45a3359352c8830927ebbef32582fc13ce5
Shubharthi Dey <deysubharthi15@gmail.com>
2021-02-22 00:34:28 +0530

    updated the algos.h

--- algos.h
+++ algos.h
@@ -1,7 +1,14 @@
+/*
+  This header file consists of all the necessary header files
+  required to run the entire program.
+*/
+
+
 #include<iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
+//List of all the required functions
 vector<int> generateVector(int,int,int);
 vector<int> manualVector(int);
 void printVector(vector<int>);
