f517f4d1524bebd601fdaa6e483cd9511907c76a
Shubharthi Dey <deysubharthi15@gmail.com>
2021-02-22 00:57:40 +0530

    updated few comments for the various sorts

--- sort.cpp
+++ sort.cpp
@@ -1,5 +1,11 @@
 #include "algos.h"
 
+/*
+    The below version of count sort 
+    implements the concept of map to keep 
+    track of each numbers frequency and 
+    place the numbers in the correct order.
+*/
 vector<int> csort(vector<int> L)
 {
     map<int,int> countMap;
@@ -17,7 +23,11 @@ vector<int> csort(vector<int> L)
     }
     return L;
 }
-
+/*
+    getMax helper function is used in 
+    Radix Sort to find out the number in the
+    vector with the largest value.
+*/
 int getMax(vector<int> L, int n)
 {
     int maximum=L.at(0);
